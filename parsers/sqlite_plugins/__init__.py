7517a6e03ec19db0dd0d9302c0ac97b0989a9e42
Samuel Choi <chjs207@gmail.com>
2020-04-23 12:39:44 +0900

    Update __init__.py

--- parsers/sqlite_plugins/__init__.py
+++ parsers/sqlite_plugins/__init__.py
@@ -1,1 +1,48 @@
+# -*- coding: utf-8 -*-
+"""Imports for the SQLite database parser."""
 
+"""
+from plaso.parsers.sqlite_plugins import android_calls
+from plaso.parsers.sqlite_plugins import android_sms
+from plaso.parsers.sqlite_plugins import android_webview
+from plaso.parsers.sqlite_plugins import android_webviewcache
+from plaso.parsers.sqlite_plugins import appusage
+from plaso.parsers.sqlite_plugins import chrome
+from plaso.parsers.sqlite_plugins import chrome_autofill
+from plaso.parsers.sqlite_plugins import chrome_cookies
+from plaso.parsers.sqlite_plugins import chrome_extension_activity
+from plaso.parsers.sqlite_plugins import firefox
+from plaso.parsers.sqlite_plugins import firefox_cookies
+from plaso.parsers.sqlite_plugins import gdrive
+from plaso.parsers.sqlite_plugins import hangouts_messages
+from plaso.parsers.sqlite_plugins import imessage
+from plaso.parsers.sqlite_plugins import kik_ios
+from plaso.parsers.sqlite_plugins import kodi
+from plaso.parsers.sqlite_plugins import ls_quarantine
+from plaso.parsers.sqlite_plugins import mac_document_versions
+from plaso.parsers.sqlite_plugins import mac_knowledgec
+from plaso.parsers.sqlite_plugins import mac_notes
+from plaso.parsers.sqlite_plugins import mac_notificationcenter
+from plaso.parsers.sqlite_plugins import mackeeper_cache
+from plaso.parsers.sqlite_plugins import safari
+from plaso.parsers.sqlite_plugins import skype
+from plaso.parsers.sqlite_plugins import tango_android
+from plaso.parsers.sqlite_plugins import twitter_android
+from plaso.parsers.sqlite_plugins import twitter_ios
+from plaso.parsers.sqlite_plugins import windows_timeline
+from plaso.parsers.sqlite_plugins import zeitgeist
+"""
+
+from plaso.parsers.sqlite_plugins import yourphone_call
+from plaso.parsers.sqlite_plugins import yourphone_message
+from plaso.parsers.sqlite_plugins import yourphone_contacts
+from plaso.parsers.sqlite_plugins import yourphone_devicedata
+from plaso.parsers.sqlite_plugins import yourphone_notifications
+from plaso.parsers.sqlite_plugins import yourphone_photos
+from plaso.parsers.sqlite_plugins import yourphone_settingsdb
+
+from plaso.parsers.sqlite_plugins import mslauncher_arrowdb
+from plaso.parsers.sqlite_plugins import mslauncher_bingsearchhistory
+from plaso.parsers.sqlite_plugins import mslauncher_notes
+from plaso.parsers.sqlite_plugins import mslauncher_activitiescache
+from plaso.parsers.sqlite_plugins import mslauncher_cookies
